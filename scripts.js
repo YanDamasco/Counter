8be6265bd676049d5373da42ded2b0b7024f4517
YanDamasco <yandamascoprofissional@gmail.com>
2025-07-17 12:36:57 -0300

    Project done. CSS and JS functions added

--- scripts.js
+++ scripts.js
@@ -1,2 +1,33 @@
-let number = document.getElementById("number").value;
+let number = document.getElementById("number");
+let decrease = document.getElementById("decreaseBtn");
+let increase = document.getElementById("increaseBtn");
+let reset = document.getElementById("resetBtn");
+let count = 0;
 
+function updateDisplay() {
+    number.textContent = count;
+    if (count < 0) {
+        number.style.color = "red";
+    } else if (count > 0){
+        number.style.color = "green";
+    } else {
+        number.style.color = "";
+    }
+}
+
+decrease.addEventListener("click", function() {
+    count--;
+    updateDisplay();
+});
+
+increase.addEventListener("click", function() {
+    count++;
+    updateDisplay();
+});
+
+reset.addEventListener("click", function() {
+    count = 0;
+    updateDisplay();
+});
+
+updateDisplay();
